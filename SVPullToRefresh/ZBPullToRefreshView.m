583a6a628803edaff0ab3b7fd13afa4b582f16d7
zhao bin <qdbin2010@foxmail.com>
2017-04-30 14:33:34 +0800

    update

--- SVPullToRefresh/ZBPullToRefreshView.m
+++ SVPullToRefresh/ZBPullToRefreshView.m
@@ -37,10 +37,28 @@
     return self;
 }
 
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
+    
+    UIColor *color = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.05];
+    [color set]; //设置线条颜色
+    
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    
+    [path moveToPoint:CGPointMake(self.bounds.size.width, 0)];
+    path.lineWidth = 1.0;
+    path.lineJoinStyle = kCGLineJoinRound; //终点处理
+    
+    [path addQuadCurveToPoint:CGPointMake(self.bounds.size.width, self.bounds.size.height) controlPoint:CGPointMake(0, self.bounds.size.height / 2)];
+    [path closePath];
+    [path stroke];
+    [path fill];
+}
+
 #pragma mark - Private
 
 - (void)initSelf {
-    self.backgroundColor = [UIColor greenColor];
+    self.backgroundColor = [UIColor clearColor];
     [self setCustomView:[[UIView alloc] init] forState:SVPullToRefreshStateAll];
 }
 
